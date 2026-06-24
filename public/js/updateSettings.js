0bd71858a6effea5b27719d400feb6941e2de5cf
Marvinpaul1 <paulmrvn@gmail.com>
2026-06-24 13:27:22 +0100

    Update settings to show picture immediately

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -16,6 +16,9 @@ export const updateSettings = async (data, type) => {
     });
     if (res.data.status === 'success') {
       showAlert('success', `${type.toUpperCase()} successfully updated`);
+      window.setTimeout(() => {
+        location.reload(true);
+      }, 1500);
     }
   } catch (error) {
     showAlert('error', error.response.data.message);
